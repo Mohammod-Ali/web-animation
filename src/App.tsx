1db493edd68c93f6dc6dd0446314b5937f97915e
Mohammod Ali <mohammodali651@gmail.com>
2024-01-23 23:47:49 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='main'>
+      <div className='box-1'>
+      {/* <h1>Animation</h1> */}
+      </div>
+    </div>
+  )
+}
+
+export default App
