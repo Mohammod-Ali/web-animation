a3bdf7956f44e5022e3e01c348537e411f5731f0
Mohammod Ali <mohammodali651@gmail.com>
2024-01-25 23:07:51 +0600

    framer motion added

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import './App.css'
+import Lesson1 from './pages/Lesson1'
 
 function App() {
 
   return (
-    <div className='main'>
-      <div className='box-1'>
+    // <div className='main'>
+    //   <div className='box-1'>
       
-      </div>
-      <div className='box-container-2'>
-        <div  className='box-2'>
+    //   </div>
+    //   <div className='box-container-2'>
+    //     <div  className='box-2'>
 
-        </div>
-      </div>
-      <h1>Animation</h1>
-    </div>
+    //     </div>
+    //   </div>
+    //   <h1>Animation</h1>
+    // </div>
+    <Lesson1></Lesson1>
   )
 }
 
