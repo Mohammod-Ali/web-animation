295c42f930aab1c834cd721d406ed8b5217541c9
Mohammod Ali <mohammodali651@gmail.com>
2024-01-26 21:14:40 +0600

    drag feature added

--- src/pages/Lesson1.tsx
+++ src/pages/Lesson1.tsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion"
+import { useRef } from "react";
 
 
 const parent = {
@@ -8,13 +9,24 @@ const parent = {
 }
 
 const Lesson1 = () => {
+    const parentRef = useRef(null)
     return (
-        <div>
+        <div
+        ref={parentRef}
+         className="border border-red-500">
             <motion.div className="size-64 bg-indigo-500  rounded-lg " 
             variants={parent}
             initial='initial'
             animate='animate'
             transition={{type: 'spring', duration: 5}}
+            drag
+            dragSnapToOrigin
+            dragElastic = {0.5}
+            dragConstraints={parentRef}
+            whileDrag={{
+                scale: 1.1,
+                boxShadow: '0px 10px 10px #000',
+            }}
             >
             
             </motion.div>
