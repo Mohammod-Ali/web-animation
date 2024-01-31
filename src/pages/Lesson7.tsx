f910f095894bc56b8c5a960427914e89ea34ef89
Mohammod Ali <mohammodali651@gmail.com>
2024-01-31 22:46:00 +0600

    useCycle  framer motion hook added

--- src/pages/Lesson7.tsx
+++ src/pages/Lesson7.tsx
@@ -1,13 +1,21 @@
 import {  motion, useCycle } from 'framer-motion';
 
 const Lesson7 = () => {
-    const [x, cycle] = useCycle(100, -100)
+
+    const boxAnimation = [
+        { x: 0, y: 0, opacity: 1},
+        { x: 100, y: 100,  opacity: 0.50},
+        { x: -100, y: 100,  opacity: 0.25},
+        { x: -100, y: -100,  opacity: 0},
+    ]
+
+    const [animate, cycle] = useCycle(...boxAnimation)
     return (
         <div
          className="border border-red-500">
             <motion.div className="size-64 bg-indigo-500  rounded-lg " 
 
-            animate={{x: x}}
+            animate={animate}
             onTap={() => cycle()}
             >
 
